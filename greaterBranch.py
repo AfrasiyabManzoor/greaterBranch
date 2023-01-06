9b824b2231753ef07edc3adf9143c657d6e05230
Afrasiyab <afrasiyabmanzoir709@gmail.com>
2023-01-06 12:21:58 +0500

    Update greaterBranch.py

--- greaterBranch.py
+++ greaterBranch.py
@@ -5,41 +5,41 @@ def solution(arr):
     if(length < 2):
         return EMPTY_STRING
     
-    totalDiff = 0
+    total_diff = 0
     i=1
     
     while pow(2,i)<length:
-        totalDiff += diffOfLevel(arr,i)
+        total_diff += diffOfLevel(arr,i)
         i+=1
     
-    if(totalDiff < 0):
+    if(total_diff < 0):
         return "Right"
-    if(totalDiff > 0):
+    if(total_diff > 0):
         return "Left"
     return EMPTY_STRING
         
     
 
 def diffOfLevel(arr, level=1): #left - right #minimum level = 1
-    totalNodes = pow(2,level)
-    index = totalNodes-1
+    total_nodes = pow(2,level)
+    index = total_nodes-1
     
-    leftSum = 0
-    rightSum = 0
+    left_sum = 0
+    right_sum = 0
     i=index
     
-    while i<index+totalNodes/2 and i<len(arr):
+    while i<index+total_nodes/2 and i<len(arr):
         if arr[i] != -1:
-            leftSum += arr[i]
+            left_sum += arr[i]
         i+=1
     
-    i=index+int(totalNodes/2)
-    while i<index+totalNodes and i<len(arr):
+    i=index+int(total_nodes/2)
+    while i<index+total_nodes and i<len(arr):
         if arr[i] != -1:
-            rightSum += arr[i]
+            right_sum += arr[i]
         i+=1
     
-    return leftSum - rightSum
+    return left_sum - right_sum
                
     
     
